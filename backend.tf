28cdbdff213e9c29f09af400b08c07bb310d83d8
Vipin Kumar Sharma <139776411+vipinsharma16@users.noreply.github.com>
2024-09-23 00:55:54 +0530

    Update backend.tf
    updated backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
-     backend "s3" {
+terraform {
+  backend "s3" {
      bucket = "terraformbackend-terraform"
      key = "terraform/terraform.tfstate"
      dynamodb_table = "terraform-state-lock-dynamo"
      region = "ap-south-1"
-   }
+  }
+}
 
