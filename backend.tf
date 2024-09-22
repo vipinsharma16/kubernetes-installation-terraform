5c5ebcce2de8ab108b674da3369a4cf8ddae87b0
Vipin Kumar Sharma <139776411+vipinsharma16@users.noreply.github.com>
2024-09-23 00:58:36 +0530

    Update backend.tf
    hash the file

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-     bucket = "terraformbackend-terraform"
-     key = "terraform/terraform.tfstate"
-     dynamodb_table = "terraform-state-lock-dynamo"
-     region = "ap-south-1"
-  }
-}
+#terraform {
+ # backend "s3" {
+ #    bucket = "terraformbackend-terraform"
+  #   key = "terraform/terraform.tfstate"
+   #  dynamodb_table = "terraform-state-lock-dynamo"
+    # region = "ap-south-1"
+  #}
+#}
 
